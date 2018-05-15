787f2ad73375890f0e7d2e67d9a430c0c0b6d0a9
aci2n <alvarocalace@hotmail.com>
2018-05-15 00:38:07 -0300

    add poreader

--- addon.py
+++ addon.py
@@ -1,71 +1,52 @@
 import urllib.request
 import urllib.parse
 import anki.utils
+from .poreader import Reader
 
 class Loader():
     def __init__(self, api):
         self.api = api
 
-    def getUrl(self, kanji, kana):
-        return 'https://assets.languagepod101.com/dictionary/japanese/audiomp3.php?' + urllib.parse.urlencode({
-            'kanji': kanji,
-            'kana': kana
-        })
-
-    def shouldSkip(self, data):
-        return anki.utils.checksum(data) == '3277fb7a67dd256e0d239e747a69d0f45cd8d1c0'
-
-    def createAudioFile(self, kanji, kana):
-        path = ''
-        url = self.getUrl(kanji, kana)
-        data = urllib.request.urlopen(url).read()
-
-        if self.shouldSkip(data):
-            print('no audio found for %s' % kanji)
-        else:
-            path = 'imported_%s_%s.mp3' % (kanji, kana)
-            self.api.saveMedia(path, data)
-            print('saved audio file for %s at %s' % (kanji, path))
-        
-        return path
-
-    def checkNote(self, id):
-        note = self.api.getNoteById(id)
-
-        if note['audio'] == '' and note['expression'] != '' and note['reading'] != '':
-            path = self.createAudioFile(note['expression'], note['reading'])
-            note['audio'] = '[sound:%s]' % path
-            note.flush()
-
-        return note
-
-    def showImportDialog(self, dialog):
-        notes = self.api.getNotesInDeck('Vocabulary::Mining')
-
-        for note in notes:
-            self.checkNote(note)
-
-        self.textEdit.setText(', '.join(map(str, notes)))
-        dialog.exec_()
-
-    def createDialog(self):
+    def createParseDialog(self):
         dialog = self.api.qt.QDialog(self.api.window)
         layout = self.api.qt.QGridLayout(dialog)
-        textEdit = self.api.qt.QTextEdit(dialog)
-
-        layout.addWidget(textEdit)
         dialog.setLayout(layout)
-        self.textEdit = textEdit
+
+        labelInput = self.api.qt.QLabel('Working directory', dialog)
+        textEditInput = self.api.qt.QTextEdit(dialog)
+        buttonInput = self.api.qt.QPushButton('Select', dialog)
+
+        buttonParse = self.api.qt.QPushButton('Parse', dialog)
+
+        layout.addWidget(labelInput, 0, 0)
+        layout.addWidget(textEditInput, 0, 1)
+        layout.addWidget(buttonInput, 0, 2)
+
+        layout.addWidget(buttonParse, 2, 1)
+
+        def buttonInputClicked():
+            textEditInput.setText(self.api.qt.QFileDialog.getExistingDirectory(dialog, 'Select the working directory...'))
+
+        buttonInput.clicked.connect(buttonInputClicked)
+
+        def parseButtonClicked():
+            directory = textEditInput.toPlainText()
+
+            if directory != '':
+                entries = Reader().read(directory)
+                file = anki.utils.os.path.join(directory, 'entries.json')
+
+                with open(file, 'w') as out:  
+                    anki.utils.json.dump(entries, out)
+
+        buttonParse.clicked.connect(parseButtonClicked)
 
         return dialog
 
-    def addAction(self, dialog):
-        action = self.api.qt.QAction("Import 蒼の彼方", self.api.window)
-        action.triggered.connect(lambda: self.showImportDialog(dialog))
+    def addParseAction(self, dialog):
+        action = self.api.qt.QAction('Parse 蒼の彼方', self.api.window)
+        action.triggered.connect(lambda: dialog.exec_())
         self.api.addToolsMenuAction(action)
 
-    def addImportAction(self):
-        self.addAction(self.createDialog())
-
     def load(self):
-        self.addImportAction()
+        self.addParseAction(self.createParseDialog())
---
+++ poreader.py
@@ -0,0 +1,22 @@
+import polib
+import glob
+import sys
+
+class Reader():
+    def read(self, directory):
+        voiceDict = {}
+        exp = directory + '/**/*.pot'
+
+        for file in glob.glob(exp):
+            entries = polib.pofile(file)
+
+            if entries != None:
+                voiceEntries = filter(lambda entry: entry.comment == 'VOICE', entries)
+                
+                for voiceEntry in voiceEntries:
+                    voiceDict[voiceEntry.msgctxt] = voiceEntry.msgstr
+
+        return voiceDict
+
+if __name__ == '__main__' and len(sys.argv) == 2:
+    print(Reader().read(sys.argv[1]))
